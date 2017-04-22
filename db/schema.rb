2471b4af4b5752c6a597c8192a0112f22ad8b1a2
aamin005 <aamin005@gmail>
2017-04-22 11:18:21 +0700

    Add portfio items via Resource generator

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170420173807) do
+ActiveRecord::Schema.define(version: 20170422041309) do
 
   create_table "blogs", force: :cascade do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "portfolios", force: :cascade do |t|
+    t.string   "title"
+    t.string   "subtitle"
+    t.text     "body"
+    t.text     "main_image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
